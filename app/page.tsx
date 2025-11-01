2e875f1c131680514f571ab738059d1edf4e1327
TANISHA3665 <jaiswaltanisha11@gmail.com>
2025-11-01 23:00:04 +0530

    added companion list

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,50 @@
-import { Button } from '@/components/ui/button';
-import React from 'react'
+import CompanionCard from '@/components/CompanionCard';
+import CompanionsList from '@/components/CompanionsList';
+import CTA from '@/components/CTA';
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text2xl underline">Welcome to my saas app</h1>
-      <Button>Click me</Button>
-    </div>
+    <main>
+      <h1>Popular Companions</h1>
+      
+      <section className='home-section'>
+        <CompanionCard
+          id='123'
+        name='Neura the Brainy Explorer'
+        topic='Neural Network of the Brain'
+        subject='science'
+        duration={45}
+        color='#E5D0FF'
+        bookmarked={true}
+        />
+
+        <CompanionCard
+          id='456'
+          name='Countsy the Number Wizard'
+          topic='Derivatives & Integrals'
+          subject='science'
+          duration={45}
+          color='#ffda6e'
+          bookmarked={true}
+        />
+
+        <CompanionCard
+          id='789'
+          name='Verba the Vocabulary Builder'
+          topic='English language'
+          subject='English Literature'
+          duration={30}
+          color='#BDE7FF'
+          bookmarked={true}
+        />
+
+      </section>
+
+      <section className='home-section'>
+        <CompanionsList />
+        <CTA/>
+      </section>
+    </main>
   );
 };
 
